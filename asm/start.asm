91c7db9e138a7cf2fee7eb969eb5e913d303680b
Champii <roidelaprog@hotmail.com>
2014-01-04 16:15:09 +0100

    updated multiboot header flags

--- asm/start.asm
+++ asm/start.asm
@@ -12,21 +12,20 @@
 global start
 start:
   mov esp, stack
-  jmp stublet
+  jmp kernel
 
 ALIGN 4
 mboot:
   MULTIBOOT_PAGE_ALIGN    equ 1<<0
-  MULTIBOOT_MEMORY_INFO   equ 1<<1
   MULTIBOOT_HEADER_MAGIC  equ 0x1BADB002
-  MULTIBOOT_HEADER_FLAGS  equ MULTIBOOT_PAGE_ALIGN | MULTIBOOT_MEMORY_INFO
+  MULTIBOOT_HEADER_FLAGS  equ MULTIBOOT_PAGE_ALIGN
   MULTIBOOT_CHECKSUM      equ -(MULTIBOOT_HEADER_MAGIC + MULTIBOOT_HEADER_FLAGS)
 
   dd MULTIBOOT_HEADER_MAGIC
   dd MULTIBOOT_HEADER_FLAGS
   dd MULTIBOOT_CHECKSUM
 
-stublet:
+kernel:
   extern main
   call main
   jmp $
@@ -41,8 +40,8 @@ flush_gdt:
     mov fs, ax
     mov gs, ax
     mov ss, ax
-    jmp 0x08:flush2
-flush2:
+    jmp 0x08:flush_ret
+flush_ret:
     ret
 
 SECTION .bss
