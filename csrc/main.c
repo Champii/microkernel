41d8972ab92064bf40b855eba55adc923a7df75b
Champii <roidelaprog@hotmail.com>
2014-01-19 21:38:56 +0100

    made an init funct to clear main

--- csrc/main.c
+++ csrc/main.c
@@ -10,23 +10,32 @@
 #include      "system.h"
 #include      "screen.h"
 #include      "gdt.h"
+#include      "mm.h"
 #include      "idt.h"
 #include      "isrs.h"
 
 
-int           main()
+void          init()
 {
   clear_screen();
 
   init_gdt();
   printk(COLOR_CYAN, "-- GDT LOADED -- !\n");
 
+  init_mm();
+  printk(COLOR_CYAN, "-- MM LOADED -- !\n");
+
   init_idt();
   printk(COLOR_CYAN, "-- IDT LOADED -- !\n");
 
   init_isrs();
   printk(COLOR_CYAN, "-- ISRS LOADED -- !\n");
+}
 
+int           main()
+{
+
+  init();
 
   printk(COLOR_RED, "Hello World !\n");
 
